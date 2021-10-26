456fc42bc85dab9c13ba72f8ece723fdc60d4b6a
404Apollo <57271403+404Apollo@users.noreply.github.com>
2021-10-26 08:40:19 -0400

    test 123

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Private from './Private';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <h1>HELLO</h1>
-    </main>
+    <div className="App">
+      <div className="page">
+        <div className="content">
+          <BrowserRouter>
+             <Header/>
+             <Route path='/' exact={true} component={Home}/>
+             <Route path='/private' exact={true} component={Private}/>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
   );
 }
 
